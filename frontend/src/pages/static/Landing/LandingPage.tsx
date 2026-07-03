0b387153f44a72b2016fd8fe8264b23da2342b37
Luka Jekic <lukajekic913@gmail.com>
2026-07-03 11:03:24 +0200

    Landing Page Text Animation

--- frontend/src/pages/static/Landing/LandingPage.tsx
+++ frontend/src/pages/static/Landing/LandingPage.tsx
@@ -18,6 +18,41 @@ const LandingPage = () => {
 
   const [btnLoading, setBtnLoading] = useState(false)
 
+  const words = ["svete.", "obrazovanju.", "budućnosti.", "Iskra."]
+  const [currentWordIndex, setCurrentWordIndex] = useState(0)
+  const [currentText, setCurrentText] = useState("")
+  const [isDeleting, setIsDeleting] = useState(false)
+  const [typingSpeed, setTypingSpeed] = useState(150)
+
+  useEffect(() => {
+    let timer;
+    const fullWord = words[currentWordIndex];
+
+    if (!isDeleting) {
+      timer = setTimeout(() => {
+        setCurrentText(fullWord.substring(0, currentText.length + 1));
+      }, typingSpeed);
+
+      if (currentText === fullWord) {
+        if (currentWordIndex === words.length - 1) {
+          return;
+        }
+        timer = setTimeout(() => setIsDeleting(true), 1200);
+      }
+    } else {
+      timer = setTimeout(() => {
+        setCurrentText(fullWord.substring(0, currentText.length - 1));
+      }, 75);
+
+      if (currentText === "") {
+        setIsDeleting(false);
+        setCurrentWordIndex((prev) => prev + 1);
+      }
+    }
+
+    return () => clearTimeout(timer);
+  }, [currentText, isDeleting, currentWordIndex]);
+
   const hadleRedirect = async()=>{
     try {
       setBtnLoading(true)
@@ -36,7 +71,12 @@ const LandingPage = () => {
     <>
     <section id="hero" className="hero pattern-bg min-h-screen w-full flex flex-col items-center justify-center p-4 text-center box-border">
         <img src="/favicon.png" className='h-[75px] mb-6' alt="" />
-        <h1 className="text-5xl md:text-8xl text-primary font-semibold mb-8">Zdravo, Iskra.</h1>
+        <h1 className="text-5xl md:text-8xl text-primary font-semibold mb-8 h-[60px] md:h-[110px]">
+            Zdravo, <span className="transition-all duration-300">{currentText}</span>
+            {currentWordIndex !== words.length - 1 && (
+              <span className="animate-pulse font-light text-muted-foreground">|</span>
+            )}
+        </h1>
         <div id="actions" className="flex flex-col sm:flex-row gap-4 justify-center items-center w-full max-w-md">
             <Button disabled={btnLoading} className="w-full sm:w-auto" onClick={()=>{hadleRedirect()}} size={'lg'}>{
               btnLoading ? (<Loader small></Loader>) : (<><ArrowUpRight/>Pristupi platformi</>)
