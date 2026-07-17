1d1df5f6ddd921adeb4c4954b61afd40e6dbb732
Luka Jekic <lukajekic913@gmail.com>
2026-07-17 19:17:38 +0200

    4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-import Sitemap from 'vite-plugin-sitemap'
 export default defineConfig({
   plugins: [react(), tailwindcss(),
-    Sitemap({ 
-      hostname: 'https://iskra-edu.vercel.app', // Zameni sa svojim domenom
-      dynamicRoutes: ['/'], // Stavili smo samo početnu stranu
-      robots: [{ userAgent: '*', allow: '/' }]
-    })
   ],
   resolve: {
     alias: {
